b88d5c926d9302b86a00829bc6f07778eb59337e
Filipe Maia <filipe.c.maia@gmail.com>
2015-02-14 01:07:15 +0100

    Separate code

--- src/translation/__init__.py
+++ src/translation/__init__.py
@@ -10,14 +10,5 @@ def init_translator(state):
         raise ValueError('Facility %s not supported' % (state['Facility']))
 
         
-class EventTranslator(object):
-    def __init__(self, event, sourceTranslator):
-        self._evt = event
-        self._trans = sourceTranslator
-        self._cache = {}
 
-    def __getitem__(self, key):
-        if key not in self._cache:
-            self._cache[key] = self._translate(self._evt, key)
-        return self._cache[key]
         
