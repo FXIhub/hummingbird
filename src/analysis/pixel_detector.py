1483b8c3b172cb770320f7bff3af9080eaab519b
N. Duane Loh <duaneloh@gmail.com>
2015-02-18 16:57:26 +0800

    Added CSPAD translation between cheetah psana/SLAC formats.

--- src/analysis/pixel_detector.py
+++ src/analysis/pixel_detector.py
@@ -7,6 +7,26 @@ def printStatistics(detectors):
                                                                 sum(v), mean(v),
                                                                 min(v), max(v),
                                                                 std(v))
-                                                              
-    
-    
+"""
+import numpy
+def slacH5ToCheetah(slacArr):
+    out_arr = numpy.zeros((8*185, 4*388))
+    for c in range(4):
+        for r in range(8):
+            slacPos = r + c*8
+            (rB, rE) = (r*185, (r+1)*185)
+            (cB, cE) = (c*388, (c+1)*388)
+            out_arr[rB:rE, cB:cE] = (slacArr[slacPos])
+    return out_arr
+
+
+def cheetahToSlacH5(cheetahArr):
+    out_arr = numpy.zeros((32, 185, 388))
+    for c in range(4):
+        for r in range(8):
+            slacPos = r + c*8
+            (rB, rE) = (r*185, (r+1)*185)
+            (cB, cE) = (c*388, (c+1)*388)
+            out_arr[slacPos] = cheetahArr[rB:rE, cB:cE]
+    return out_arr
+"""
