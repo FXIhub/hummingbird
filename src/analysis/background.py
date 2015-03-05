d9e600cd32625a943bf5f87b5396a58e6645ae12
daurer <benedikt.daurer@icm.uu.se>
2015-03-05 12:48:15 -0800

    modifications to the mean photon plot

--- src/analysis/background.py
+++ src/analysis/background.py
@@ -6,24 +6,28 @@ from backend import Backend
 
 class MeanPhotonMap:
     def __init__(self, conf):
-        #global photonMap, photonMapX, photonMapY, photonMapN
         xmin, xmax = conf["paramXmin"], conf["paramXmax"]
         ymin, ymax = conf["paramYmin"], conf["paramYmax"]
         xbin, ybin = conf["paramXbin"], conf["paramYbin"]
         self.photonMapX = numpy.linspace(xmin, xmax, (xmax-xmin)/float(xbin) + 1)
         self.photonMapY = numpy.linspace(ymin, ymax, (ymax-ymin)/float(ybin) + 1)
-        self.photonMap = numpy.zeros((self.photonMapY.shape[0], self.photonMapX.shape[0]))
-        self.photonMapN = 0
+        self.photonMap  = numpy.zeros((self.photonMapY.shape[0], self.photonMapX.shape[0]), dtype=numpy.float)
+        self.eventMap   = numpy.zeros((self.photonMapY.shape[0], self.photonMapX.shape[0]), dtype=numpy.float)
+        self.meanMap    = numpy.zeros((self.photonMapY.shape[0], self.photonMapX.shape[0]), dtype=numpy.float)
 
     def append(self, N, X, Y):
         self.photonMap[abs(self.photonMapY - Y.data).argmin(), abs(self.photonMapX - X.data).argmin()] += N
-        self.photonMapN += 1
+        self.eventMap[abs(self.photonMapY - Y.data).argmin(), abs(self.photonMapX - X.data).argmin()] += 1.
+        visited = self.eventMap != 0
+        self.meanMap[visited] = self.photonMap[visited] / self.eventMap[visited]
+        self.meanMap[~visited] = 1.1 * self.meanMap[visited].max()
 
 photonMaps = {}
 def plotMeanPhotonMap(key, conf, nrPhotons, paramX, paramY):
     if not key in photonMaps:
         photonMaps[key] = MeanPhotonMap(conf)
-    for k,m in photonMaps.iteritems():
-        m.append(nrPhotons, paramX, paramY)
-        if(not m.photonMapN % conf["updateRate"]):
-            ipc.new_data('meanPhotonMap_%s_%s' %(paramX.name, paramY.name), m.photonMap/float(m.photonMapN))
+    m = photonMaps[key]
+    m.append(nrPhotons, paramX, paramY)
+    if(not m.eventMap.sum() % conf["updateRate"]):
+        print m.meanMap.shape
+        ipc.new_data('meanPhotonMap_%s' %key, m.meanMap)
