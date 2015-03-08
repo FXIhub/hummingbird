9eb66949b3ef8cff08f9b61aafd6af9965fcc7e0
daurer <benedikt.daurer@icm.uu.se>
2015-03-08 10:29:52 -0700

    polished meanPhotonMap

--- src/analysis/hitfinding.py
+++ src/analysis/hitfinding.py
@@ -21,49 +21,88 @@ class MeanPhotonMap:
     def __init__(self, key, conf):
         xmin, xmax = conf["xmin"],  conf["xmax"]
         ymin, ymax = conf["ymin"],  conf["ymax"]
-        step = conf["step"]
-        radius = conf["radius"]
-        self.photonMapX = numpy.linspace(xmin, xmax, (xmax-xmin)/float(step))
-        self.photonMapY = numpy.linspace(ymin, ymax, (ymax-ymin)/float(step))
-        Nx = self.photonMapX.shape[0]
-        Ny = self.photonMapY.shape[0]
-        self.photonMap  = lil_matrix((Ny, Nx), dtype=numpy.float32)
-        self.gasMap     = lil_matrix((Ny, Nx), dtype=numpy.float32)
-        #self.eventMap   = lil_matrix((Ny, Nx), dtype=numpy.bool)
-        #self.eventMapDown = np.zeros((100,100)), dtype=numpy.bool)
-        self.counter    = 0
-        #ipc.broadcast.init_data(key+'overview', data_type='image', xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, xlabel=conf["xlabel"], ylabel=conf["ylabel"], history_length=1)
-        #print Nx,Ny
-        #print Nx/2-radius, Nx/2+radius
-        #print Ny/2-radius, Ny/2+radius
-        ipc.broadcast.init_data(key+'local', data_type='image', xmin=self.photonMapX[Nx/2-radius], xmax=self.photonMapX[Nx/2+radius+1], ymin=self.photonMapY[Ny/2-radius], ymax=self.photonMapY[Ny/2+radius+1], xlabel=conf["xlabel"], ylabel=conf["ylabel"], history_length=10)
+        step   = conf["step"]
+        self.radius = conf["radius"]
+        gridstep = conf["gridstep"]
+        
+        self.xrange = numpy.linspace(xmin, xmax, (xmax-xmin)/float(step))
+        self.yrange = numpy.linspace(ymin, ymax, (ymax-ymin)/float(step))
+        Nx = self.xrange.shape[0]
+        Ny = self.yrange.shape[0]
+        self.localXmin = self.xrange[Nx/2-self.radius]
+        self.localXmax = self.xrange[Nx/2+self.radius+1]
+        self.localYmin = self.yrange[Ny/2-self.radius]
+        self.localYmax = self.yrange[Ny/2+self.radius+1]
+        self.integralMap  = lil_matrix((Ny, Nx), dtype=numpy.float32)
+        self.normMap      = lil_matrix((Ny, Nx), dtype=numpy.float32)
+        self.localMeanMap      = numpy.zeros((2*self.radius, 2*self.radius))
 
-    def append(self, N, X, Y, G):
-        self.photonMap[abs(self.photonMapY - Y.data).argmin(), abs(self.photonMapX - X.data).argmin()] += N
-        self.gasMap[abs(self.photonMapY - Y.data).argmin(), abs(self.photonMapX - X.data).argmin()] += G
-        #self.eventMap[abs(self.photonMapY - Y.data).argmin(), abs(self.photonMapX - X.data).argmin()] = 1
-        #visited = self.eventMap != 0
-        #self.meanMap[visited] = self.photonMap[visited] / self.eventMap[visited]
-        #self.meanMap[~visited] = 1.1 * self.meanMap[visited].max()
-        #self.meanMap[~visited] = 0.0
+        self.gridxrange = numpy.linspace(xmin, xmax, (xmax-xmin)/float(gridstep))
+        self.gridyrange = numpy.linspace(ymin, ymax, (ymax-ymin)/float(gridstep))
+        gridNx = self.gridxrange.shape[0]
+        gridNy = self.gridyrange.shape[0]
+        #self.gridIntegralMap = numpy.zeros((gridNy, gridNx))
+        #self.gridNormMap     = numpy.zeros((gridNy, gridNx))
+        self.gridMap = numpy.zeros((gridNy, gridNx))
+        
+        self.counter    = 0
+        ipc.broadcast.init_data(key+'overview', data_type='image', xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, xlabel=conf["xlabel"], ylabel=conf["ylabel"], history_length=1, flipy=True)
+        ipc.broadcast.init_data(key+'local', data_type='image', xmin=self.localXmin, xmax=self.localXmax, ymin=self.localYmin, ymax=self.localYmax, xlabel=conf["xlabel"], ylabel=conf["ylabel"], history_length=1, flipy=True)
+
+    def append(self, X, Y, I, N):
+        self.integralMap[abs(self.yrange - Y.data).argmin(), abs(self.xrange - X.data).argmin()] += I
+        self.normMap[abs(self.yrange - Y.data).argmin(), abs(self.xrange - X.data).argmin()] += N
+        #self.gridIntegralMap[abs(self.gridyrange - Y.data).argmin(), abs(self.gridxrange - X.data).argmin()] += I
+        #self.gridNormMap[abs(self.gridyrange - Y.data).argmin(), abs(self.gridxrange - X.data).argmin()] += N
+        self.gridMap[abs(self.gridyrange - Y.data).argmin(), abs(self.gridxrange - X.data).argmin()] += 1
         self.counter += 1
 
-def grab_array(A, cen, rad):
-    return A[cen[0]-rad: cen[0]+rad+1, cen[1]-rad:cen[1]+rad+1].toarray()
+    def update_center(self, X, Y):
+        self.center = (abs(self.yrange - Y.data).argmin(), abs(self.xrange - X.data).argmin())
+
+    def update_local_limits(self):
+        self.localXmin = self.xrange[self.center[1]-self.radius]
+        self.localXmax = self.xrange[self.center[1]+self.radius+1]
+        self.localYmin = self.yrange[self.center[0]-self.radius]
+        self.localYmax = self.yrange[self.center[0]+self.radius+1]
+
+    def update_local_maps(self):
+        rad = self.radius
+        self.localIntegralMap = self.integralMap[self.center[0]-rad: self.center[0]+rad+1, self.center[1]-rad:self.center[1]+rad+1].toarray()
+        self.localNormMap     = self.normMap[self.center[0]-rad: self.center[0]+rad+1, self.center[1]-rad:self.center[1]+rad+1].toarray()
+        visited = self.localNormMap != 0
+        self.localMeanMap[visited] = self.localIntegralMap[visited] / self.localNormMap[visited]
+
+    def update_gridmap(self, X,Y):
+        current = (abs(self.gridyrange - Y.data).argmin(), abs(self.gridxrange - X.data).argmin())
+        visited = self.gridMap != 0
+        self.gridMap[visited] = 1
+        self.gridMap[current] = 2
 
 photonMaps = {}
 def plotMeanPhotonMap(key, conf, nrPhotons, paramX, paramY, pulseEnergy):
     if not key in photonMaps:
         photonMaps[key] = MeanPhotonMap(key,conf)
     m = photonMaps[key]
-    m.append(nrPhotons, paramX, paramY, pulseEnergy)
+    m.append(paramX, paramY, nrPhotons, pulseEnergy)
     if(not m.counter % conf["updateRate"]):
-        center = (abs(m.photonMapY - paramY.data).argmin(), abs(m.photonMapX - paramX.data).argmin())
-        photonMap = grab_array(m.photonMap, center, conf["radius"])
-        gasMap = grab_array(m.gasMap, center, conf["radius"])
-        visited = gasMap != 0
-        meanMap = numpy.zeros(photonMap.shape)
-        meanMap[visited] = photonMap[visited] / gasMap[visited]
+        m.update_center(paramX, paramY)
+        m.update_local_limits()
+        m.update_local_maps()
+        m.update_gridmap(paramX,paramY)
+        
+        ipc.new_data(key+'overview', m.gridMap) 
+        ipc.new_data(key+'local', m.localMeanMap, xmin=m.localXmin, xmax=m.localXmax, ymin=m.localYmin, ymax=m.localYmax)
+        
+
+        #center = (abs(m.photonMapY - paramY.data).argmin(), abs(m.photonMapX - paramX.data).argmin())
+        #photonMap = grab_array(m.photonMap, center, conf["radius"])
+        #gasMap = grab_array(m.gasMap, center, conf["radius"])
+
+        
+        
+        #meanMap = numpy.zeros(photonMap.shape)
+        #meanMap[visited] = photonMap[visited] / gasMap[visited]
         #print m.eventMap.shape
         #Nx = 100
         #Ny = 100
@@ -83,7 +122,7 @@ def plotMeanPhotonMap(key, conf, nrPhotons, paramX, paramY, pulseEnergy):
         #print lx,ly,x[lx[0]], y[ly[0]]
         #ly, lx = numpy.where(eventMap.T == 1)
         #print lx,ly,x[lx[0]], y[ly[0]]
-        rad = conf["radius"]
+        #rad = conf["radius"]
         #print eventMap.shape
         #eventMap = numpy.transpose(numpy.asarray(eventMap))
         #ly, lx = numpy.where(eventMap == 1)
@@ -93,8 +132,7 @@ def plotMeanPhotonMap(key, conf, nrPhotons, paramX, paramY, pulseEnergy):
         #print type(eventMap)
         #eventMap2 = numpy.zeros(eventMap.shape)
         #eventMap2[:] = eventMap.T
-        #ipc.new_data(key+'overview', eventMap.T) 
-        ipc.new_data(key+'local', meanMap, xmin=m.photonMapX[center[1]-rad], xmax=m.photonMapX[center[1]+rad+1], ymin=m.photonMapY[center[0]-rad], ymax=m.photonMapY[center[0]+rad+1])
+        
     #minimum = m.meanMap.min()
     #print minimum
     #yopt, xopt = numpy.where(m.meanMap == minimum)
