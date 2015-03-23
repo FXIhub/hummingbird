2fd3b660ca3d25419f42219af26acf3052458d3c
Filipe Maia <filipe.c.maia@gmail.com>
2015-03-23 15:09:01 +0100

    Finished cleaning backend package

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -1,16 +1,12 @@
-import logging
+"""Implements all the reading and translation of facility specific
+data streams into a unified format that can be used by the analysis
+package."""
 
 from .pint import UnitRegistry
-from .worker import Worker 
-from .event_translator import EventTranslator
-from .record import Record, addRecord
+from .worker import Worker # pylint: disable=unused-import
+from .event_translator import EventTranslator # pylint: disable=unused-import
+from .record import Record, addRecord # pylint: disable=unused-import
 
 ureg = UnitRegistry()
 ureg.enable_contexts('spectroscopy')
 ureg.default_format = '~'
-
-
-
-        
-
-        
