3d43bd1322e8df14a219cf48562153adc0551d83
Filipe Maia <filipe.c.maia@gmail.com>
2015-03-23 17:53:01 +0100

    Clear PlotData

--- src/interface/plotdata.py
+++ src/interface/plotdata.py
@@ -1,40 +1,38 @@
-from ringbuffer import RingBuffer
+"""Stores the data associated with a given broadcast"""
+from interface.ringbuffer import RingBuffer
 import numpy
-from Qt import QtGui, QtCore
-from ui import PlotWindow
 
-class PlotData(QtCore.QObject):
-    def __init__(self, parent, title, maxlen = 1000):
-        QtCore.QObject.__init__(self,parent)
+class PlotData(object):
+    """Stores the data associated with a given broadcast"""
+    def __init__(self, parent, title, maxlen=1000):
         self._title = title
-        self._y = None
-        self._x = None
+        self._y = None # pylint: disable=invalid-name
+        self._x = None # pylint: disable=invalid-name
         self._widget = None
         self._parent = parent
         self._maxlen = maxlen
         if('history_length' in parent.conf[title]):
             self._maxlen = parent.conf[title]['history_length']
 
-    def set_data(self, yy):
+    def set_data(self, data):
+        """Clear the ringbuffers and fills them with the given data"""
         if(self._y is None):
             self._y = RingBuffer(self._maxlen)
         else:
             self._y.clear()
-        for y in yy:
+        for y in data:
             self._y.append(y)
 
     def append(self, y, x):
+        """Append the new data to the ringbuffers"""
         if(self._y is None):
-            if(isinstance(y,numpy.ndarray)):
+            if(isinstance(y, numpy.ndarray)):
                 # Make sure the image ringbuffers don't take more than
                 # 200 MBs. The factor of 2 takes into account the fact
                 # that the buffer is twice as big as its usable size
-                self._y = RingBuffer(max(1,min(self._maxlen,
-                                               1024*1024*200/(2*y.nbytes))))
-            else:
-                self._y = RingBuffer(self._maxlen) 
+                self._maxlen = max(1, min(self._maxlen, 1024*1024*200/(2*y.nbytes)))
+            self._y = RingBuffer(self._maxlen)
         if(self._x is None):
-            self._x = RingBuffer(self._y._maxlen)
-
-        self._y.append(y)            
+            self._x = RingBuffer(self._maxlen)
+        self._y.append(y)
         self._x.append(x)
