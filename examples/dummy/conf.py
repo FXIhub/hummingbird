007d608b952cf31a4fa67a8e9c04dc7323c59f04
Filipe Maia <filipe.c.maia@gmail.com>
2015-04-23 17:06:14 +0200

    Provide some commentary documentation about the configurable dummy backend

--- examples/dummy/conf.py
+++ examples/dummy/conf.py
@@ -11,11 +11,21 @@ state = {
     'Facility': 'Dummy',
 
     'Dummy': {
+        # The event repetition rate of the facility 
         'Repetition Rate' : 1,
+        # Dictionary of data sources
         'Data Sources': {
+            # The name of the data source. This is the key under which it will be found
+            # when iterating over members of its type.
+            # It's also the native key that will be used
             'CCD': {
+                # A function that will generate the data for every event
                 'data': lambda: numpy.random.rand(256,128),
-                'unit': ureg.ADU,                                       
+                # The units to be used
+                'unit': ureg.ADU,     
+                # The name of the category for this data source.
+                # All data sources are aggregated by type, which is the key
+                # used when asking for them in the analysis code.
                 'type': 'photonPixelDetectors'
             }
         }        
