b033eeabb4d32df2613ec14b35a89a3f51cbf33e
Filipe Maia <filipe.c.maia@gmail.com>
2015-04-23 17:14:37 +0200

    Correct example to use configurable backend

--- examples/test/conf.py
+++ examples/test/conf.py
@@ -4,29 +4,49 @@ import analysis.beamline
 import analysis.background
 import analysis.pixel_detector
 import ipc
+import random
+from backend import ureg
+import numpy
 
 state = {
     'Facility': 'dummy',
     'squareImage' : True,
+    'Dummy': {
+        'Repetition Rate' : 1,
+        'Data Sources': {
+            'CCD': {
+                'data': lambda: numpy.random.rand(256,128),
+                'unit': ureg.ADU,     
+                'type': 'photonPixelDetectors'
+            },
+            'CCD1': {
+                'data': lambda: numpy.random.rand(64,64),
+                'unit': ureg.ADU,     
+                'type': 'photonPixelDetectors'
+            },
+            'tof': {
+                'data': lambda: numpy.random.rand(256),
+                'unit': ureg.mJ,
+                'type': 'ionTOFs'
+            },
+            'pulseEnergy1': {
+                'data': lambda: random.random(),
+                'unit': ureg.mJ,
+                'type': 'pulseEnergies'
+            }
+        }        
+    }
 }
 
 
 def onEvent(evt):
-#    ipc.broadcast.init_data('CCD', xmin=10,ymin=10, xmax=20,ymax=30, xlabel='foo', ylabel='bar')
-    t0 = time.time()
-#    ipc.new_data('CCD2', evt['photonPixelDetectors']['CCD2'].data)
+
     ipc.new_data('CCD', evt['photonPixelDetectors']['CCD'].data, xlabel='x', ylabel='y', xmax=112.0, xmin=100, ymin=-50)
     ipc.new_data('CCD_flipped', evt['photonPixelDetectors']['CCD'].data, flipy=True, xlabel='x', ylabel='y', xmax=112.0, xmin=100, ymin=-50)
     ipc.new_data('CCD_tranposed', evt['photonPixelDetectors']['CCD'].data, transpose=True, xlabel='x', ylabel='y', xmax=112.0, xmin=100, ymin=-50)
     ipc.new_data('CCD_noisy', evt['photonPixelDetectors']['CCD1'].data)
     ipc.new_data('tof', evt['ionTOFs']['tof'].data, xlabel='foo')
-    t1 = time.time()
-#    print 1.0/(t1-t0)
-#    analysis.pixel_detector.plotImages(evt['photonPixelDetectors'])
     ipc.broadcast.init_data('pulseEnergy1', xlabel='foo', ylabel='bar2', history_length=3)
     ipc.new_data('pulse1', evt['pulseEnergies']['pulseEnergy1'].data)
     ipc.new_data('pulse3', evt['pulseEnergies']['pulseEnergy1'].data, xlabel='foo')
-#    analysis.beamline.plotPulseEnergy(evt['pulseEnergies'])
-    #analysis.pixel_detector.printStatistics(evt['photonPixelDetectors'])
     analysis.event.printProcessingRate(evt)
-    time.sleep(1)
