fdbe5e1e92183030555295f4c73bba6fb5f54220
Benedikt Daurer <benedikt.daurer@icm.uu.se>
2015-05-15 11:05:10 -0700

    Move psana examples in separate folder

---
+++ examples/psana/xtc/conf.py
@@ -0,0 +1,17 @@
+import analysis.event
+import analysis.beamline
+import analysis.pixel_detector
+   
+state = {
+    'Facility': 'LCLS',
+    'LCLS/DataSource': '/data/rawdata/cxic9714/xtc/e419-r0203-s00-c00.xtc'
+}
+   
+def onEvent(evt):
+    analysis.beamline.printPulseEnergy(evt['pulseEnergies'])
+    analysis.beamline.printPhotonEnergy(evt['photonEnergies'])
+    print "EPICS photon energy = %g eV" %(evt['parameters']['SIOC:SYS0:ML00:AO541'].data)
+    analysis.pixel_detector.printStatistics(evt['photonPixelDetectors'])
+    analysis.pixel_detector.printStatistics(evt['ionTOFs'])
+    analysis.event.printID(evt['eventID'])
+    analysis.event.printProcessingRate(evt)
