Fixed plot trace - radial averages now visible in examples/sizing_radial/conf.py

src/plotting/line.py
@@ -53,10 +53,10 @@ def plotTrace(paramY, paramX=None, label='', history=100):
     """Plotting a trace.
     
     Args:
-        :paramY(Record):   The data for the ordinate is paramY.data.flatten()
+        :paramY(Record):   The data for the ordinate is paramY.data.ravel()
 
     Kwargs:
-        :paramX(Record):   The data for the abscissa is paramX.data.flatten() if paramX is not None
+        :paramX(Record):   The data for the abscissa is paramX.data.ravel() if paramX is not None
         :label(str):    Label for param
         :history(int):  Length of history buffer
     """
@@ -67,7 +67,12 @@ def plotTrace(paramY, paramX=None, label='', history=100):
         ipc.broadcast.init_data(plotid, data_type='vector', xlabel=label, history_length=history)
         histograms[paramY.name] = True
     if paramX is None:
-        ipc.new_data(plotid, data_y=paramY.data.flatten())
+        ipc.new_data(plotid, data_y=paramY.data.ravel())
     else:
-        ipc.new_data(plotid, data_y=paramY.data.flatten(), data_x=paramX.data.flatten())
+        x = paramX.data.ravel()
+        y = paramY.data.ravel()
+        if x.size != y.size:
+            logging.warning("For %s x- and y-dimension do not match (%i, %i). Cannot plot trace." % (plotid,x.size,y.size))
+            return
+        ipc.new_data(plotid, data_y=np.array([x,y], copy=False)) 
 