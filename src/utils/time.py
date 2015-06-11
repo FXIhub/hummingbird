c412c60d134ca9626ccfdfced6df34106e6f4721
Benedikt Daurer <benedikt.daurer@icm.uu.se>
2015-06-11 15:51:34 -0700

    Fixed the bit shifting problem

--- src/utils/time.py
+++ src/utils/time.py
@@ -2,10 +2,7 @@ import struct
 import numpy
 
 def lcls2float(t):
-   #if isinstance(t, numpy.number):
    print t, type(t)
-   t0 = numpy.right_shift(t, 32)
-   #else:
-   #   t0 = t >> 32
+   t0 = t >> 32
    t1 = t & 0x00000000fffffffff
    return t0 + t1*1e-9
