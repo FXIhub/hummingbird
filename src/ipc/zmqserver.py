7398deca852ca70dc7a3be6781bb5d4f2e1aaee5
Filipe Maia <filipe.c.maia@gmail.com>
2015-08-02 11:15:22 -0700

    Detect when clients subscribe to data sources

--- src/ipc/zmqserver.py
+++ src/ipc/zmqserver.py
@@ -8,35 +8,45 @@ import ipc
 import numpy
 import hashlib
 import ipc.mpi
+import backend.worker
+import logging
 
 class ZmqServer(object):
     """Implements the server that broadcasts the results from the backend.
     Analysis users do not need to deal with it."""
     def __init__(self):
+        self._state = backend.worker.Worker.state
         self._zmq_key = bytes('hummingbird')
         self._context = zmq.Context()
-        self._data_socket = self._context.socket(zmq.PUB)
-        self._data_port = 13132
-        self._data_socket.setsockopt(zmq.SNDHWM, 10)
         self._ctrl_socket = self._context.socket(zmq.REP)
-        while True:
-            try:
-                self._data_socket.bind("tcp://*:%d" % (self._data_port))
-                self._ctrl_socket.bind("tcp://*:%d" % (self._data_port - 1))
-                print "ZMQ port is set to: %d" % self._data_port
-                break
-            except zmq.error.ZMQError:
-                print "ZMQ port in use: %d" % self._data_port
-                raw_string = raw_input("Enter new port [leave blank to try next one]: ")
-                if raw_string == '':
-                    self._data_port += 2
-                else:
-                    self._data_port = int(raw_string)
-                    #self._data_port = int(raw_string.rstrip())
+        self._ctrl_port = self._state.get('zmq_ctrl_port',13131)
+        self._broker_pub_socket = self._context.socket(zmq.XPUB)
+        self._broker_pub_port = self._state.get('zmq_data_port',
+                                                self._ctrl_port+1)
+        self._broker_sub_socket = self._context.socket(zmq.XSUB)
+        self._broker_sub_port = self._state.get('zmq_xsub_port', 
+                                                self._broker_pub_port+1)
+
+        self._data_socket = self._context.socket(zmq.PUB)
+        self._data_socket.setsockopt(zmq.SNDHWM, 10)
+        self._ctrl_socket.bind("tcp://*:%d" % (self._ctrl_port))
+        self._broker_pub_socket.bind("tcp://*:%d" % (self._broker_pub_port))
+        self._broker_sub_socket.bind("tcp://*:%d" % (self._broker_sub_port))
+        self._data_socket.connect("tcp://127.0.0.1:%d" % (self._broker_sub_port))
+
         zmq.eventloop.ioloop.install()
         self._ctrl_stream = zmq.eventloop.zmqstream.ZMQStream(self._ctrl_socket)
         self._ctrl_stream.on_recv_stream(self._answer_command)
-        ipc.uuid = ipc.hostname+':'+str(self._data_port)
+
+        self._xpub_stream = zmq.eventloop.zmqstream.ZMQStream(self._broker_pub_socket)
+        self._xpub_stream.on_recv_stream(self._forward_xpub)
+
+        self._xsub_stream = zmq.eventloop.zmqstream.ZMQStream(self._broker_sub_socket)
+        self._xsub_stream.on_recv_stream(self._forward_xsub)
+
+        self._subscribed = set()
+
+        ipc.uuid = ipc.hostname+':'+str(self._broker_pub_port)
         t = threading.Thread(target=self._ioloop)
         # Make sure the program exists even when the thread exists
         t.daemon = True
@@ -87,7 +97,7 @@ class ZmqServer(object):
         if(msg[0] == 'conf'):
             stream.socket.send_json(['conf', ipc.broadcast.data_conf])
         if(msg[0] == 'data_port'):
-            stream.socket.send_json(['data_port', bytes(self._data_port)])
+            stream.socket.send_json(['data_port', bytes(self._broker_pub_port)])
         if(msg[0] == 'uuid'):
             stream.socket.send_json(['uuid', bytes(ipc.uuid)])
         if(msg[0] == 'reload'):
@@ -101,3 +111,21 @@ class ZmqServer(object):
         on the control stream. Runs on a separate thread."""
         zmq.eventloop.ioloop.IOLoop.instance().start()
         print "ioloop ended"
+
+    def _forward_xsub(self, stream, msg):
+        self._xpub_stream.send_multipart(msg)
+
+    def _forward_xpub(self, stream, msg):
+        if msg[0][0] == '\x00':
+            logging.debug("Got unsubscription for: %r" % msg[0])
+            self._subscribed.discard(msg[0])
+        elif msg[0][0] == '\x01':
+            logging.debug("Got subscription for: %r" % msg[0])
+            self._subscribed.add(msg[0])
+        else:
+            raise ValueError('Unexpected message: %r' % msg[0])
+        self._xsub_stream.send_multipart(msg)
+
+    @property
+    def subscribed(self):
+        return self._subscribed
