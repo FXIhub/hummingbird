f5be9131880ea33a755133fefff6122faf1876cb
Benedikt Daurer <benedikt.daurer@icm.uu.se>
2015-11-18 17:49:35 +0100

    Updated copyright info

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SPIhub Online Analysis'
-copyright = u'2015, Single Particle Initiative Team'
+copyright = u'2015, SPIhub Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
