bf4f972d5423a0a00a85019b1403b8fd84ac0c97
Benedikt Daurer <benedikt.daurer@icm.uu.se>
2016-01-28 19:18:09 +0100

    Fixing examples: basic examples are tested

---
+++ examples/basic/detector.py
@@ -0,0 +1,78 @@
+# Import analysis/plotting/simulation modules
+import analysis.event
+import analysis.pixel_detector
+import plotting.line
+import plotting.image
+import simulation.base
+
+# Simulate diffraction data  
+sim = simulation.base.Simulation()
+sim.hitrate = 0.5
+sim.sigma = 1
+
+# Specify the facility
+state = {}
+state['Facility'] = 'Dummy'
+
+# Create a dummy facility
+state['Dummy'] = {
+    # The event repetition rate of the dummy facility [Hz]
+    'Repetition Rate' : 10,
+    # Specify simulation
+    'Simulation': sim,
+    # Dictionary of data sources
+    'Data Sources': {
+        # Data from a virtual diffraction detector
+        'CCD': {
+            # Fetch diffraction data from the simulation
+            'data': sim.get_pattern,
+            'unit': 'ADU',
+            'type': 'photonPixelDetectors'
+        },
+        # Data from a virutal pulse energy detector
+        'pulseEnergy': {
+            # Fetch pulse energy valus from the simulation
+            'data': sim.get_pulse_energy,
+            'unit': 'J',
+            'type': 'pulseEnergies'
+        }
+    }
+}
+
+# This function is called for every single event
+# following the given recipy of analysis
+def onEvent(evt):
+
+    # Processing rate [Hz]
+    analysis.event.printProcessingRate()
+
+    # Visualize detector image
+    plotting.image.plotImage(evt['photonPixelDetectors']['CCD'], vmin=-10, vmax=40)
+
+# Configuration for histogram plot
+histogramCCD = {
+    'hmin': -10,
+    'hmax': 100,
+    'bins': 200,
+    'label': "Nr of photons",
+    'history': 200}
+
+# This function is called for every single event
+# following the given recipy of analysis
+def onEvent(evt):
+
+    # Processing rate [Hz]
+    analysis.event.printProcessingRate()
+
+    # Detector statistics
+    analysis.pixel_detector.printStatistics(evt["photonPixelDetectors"])
+
+    # Count Nr. of Photons
+    analysis.pixel_detector.totalNrPhotons(evt, "photonPixelDetectors", "CCD")
+    plotting.line.plotHistory(evt["analysis"]["nrPhotons - CCD"], label='Nr of photons / frame', history=50)
+
+    # Detector histogram
+    plotting.line.plotHistogram(evt["photonPixelDetectors"]["CCD"], **histogramCCD)
+ 
+    # Detector images
+    plotting.image.plotImage(evt["photonPixelDetectors"]["CCD"])
---
+++ examples/basic/simulation.py
@@ -0,0 +1,48 @@
+# Import analysis/plotting/simulation modules
+import analysis.event
+import plotting.image
+import simulation.base
+
+# Simulate diffraction data  
+sim = simulation.base.Simulation()
+sim.hitrate = 0.5
+sim.sigma = 1
+
+# Specify the facility
+state = {}
+state['Facility'] = 'Dummy'
+
+# Create a dummy facility
+state['Dummy'] = {
+    # The event repetition rate of the dummy facility [Hz]
+    'Repetition Rate' : 10,
+    # Specify simulation
+    'Simulation': sim,
+    # Dictionary of data sources
+    'Data Sources': {
+        # Data from a virtual diffraction detector
+        'CCD': {
+            # Fetch diffraction data from the simulation
+            'data': sim.get_pattern,
+            'unit': 'ADU',
+            'type': 'photonPixelDetectors'
+        },
+        # Data from a virutal pulse energy detector
+        'pulseEnergy': {
+            # Fetch pulse energy valus from the simulation
+            'data': sim.get_pulse_energy,
+            'unit': 'J',
+            'type': 'pulseEnergies'
+        }
+    }
+}
+
+# This function is called for every single event
+# following the given recipy of analysis
+def onEvent(evt):
+
+    # Processing rate [Hz]
+    analysis.event.printProcessingRate()
+
+    # Visualize detector image
+    plotting.image.plotImage(evt['photonPixelDetectors']['CCD'], vmin=-10, vmax=40)
