538582706944e2e29fc618a241947db86221da9a
Benedikt Daurer <benedikt.daurer@icm.uu.se>
2016-02-16 20:17:21 +0100

    Re-arranged lcls examples

---
+++ examples/lcls/mimi_dark.py
@@ -0,0 +1,21 @@
+import analysis.event
+import plotting.image
+import h5py
+
+state = {}
+state['Facility'] = 'LCLS'
+state['LCLS/DataSource'] = 'exp=amo15010:dir=/reg/d/psdm/AMO/amo15010/xtc/:run=73'
+
+dark = 0.
+event_number = 0
+
+def onEvent(evt):
+
+    global dark, event_number
+    dark  += evt['photonPixelDetectors']['pnccdBackfullFrame'].data
+    event_number += 1
+
+def end_of_run():
+    print "Saving average dark image to dark_run73.h5"
+    with h5py.File('dark_run73.h5', 'w') as f:
+        f['mean']  = dark  / event_number
