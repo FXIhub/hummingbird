942e47b2ae9d0b707308a9f26078226869220cdc
Benedikt Daurer <benedikt.daurer@icm.uu.se>
2016-03-28 21:38:07 +0200

    Employ new (unified) style to beamline modules

--- src/analysis/beamline.py
+++ src/analysis/beamline.py
@@ -8,20 +8,26 @@ import numpy as np
 from backend import  ureg
 from backend import add_record
 
-def averagePulseEnergy(evt, type):
-    """Averages pulse energies. Expects an event and an event type and adds the
-    average pulse energy to ``evt["analysis"]["averagePulseEnergy"]``.
-    
+def averagePulseEnergy(evt, records, outkey="averagePulseEnergy"):
+    """Averages across given pulse energies and adds it to evt["analysis"][outkey].
+
+    Args:
+        :evt:      The event variable
+        :records:  A dictionary of pulse energy ``Records``
+
+    Kwargs:
+        :outkey(str):  Data key of resulting ``Record``, default is 'averagePulseEnergy'
+
     :Authors:
         Filipe Maia
+        Benedikt J. Daurer
     """
-    pulseEnergies = evt[type]
     pulseEnergy = []
-    for pE in pulseEnergies.values():
+    for pE in records.values():
         if (pE.unit == ureg.mJ):
             pulseEnergy.append(pE.data)
     if pulseEnergy:
-        add_record(evt["analysis"], "analysis", "averagePulseEnergy", np.mean(pulseEnergy), ureg.mJ)
+        add_record(evt["analysis"], "analysis", outkey, np.mean(pulseEnergy), ureg.mJ)
 
 def printPulseEnergy(pulseEnergies):
     """Expects a dictionary of pulse energy ``Records`` and prints pulse energies to screen."""
