8df3b99b47fbd1d553386a7a06684e03975561f9
cnettel <carl.nettelblad@icm.uu.se>
2016-03-31 10:33:07 -0700

    Testing #66, qApp global variable

--- src/interface/__init__.py
+++ src/interface/__init__.py
@@ -21,7 +21,7 @@ def sigint_handler(*_):
     if QtGui.QMessageBox.question(None, '', "Are you sure you want to quit?",
                                   QtGui.QMessageBox.Yes | QtGui.QMessageBox.No,
                                   QtGui.QMessageBox.No) == QtGui.QMessageBox.Yes:
-        QtGui.QApplication.instance.quit()
+        QtGui.qApp.quit()
 
 
 from interface.data_source import DataSource # pylint: disable=unused-import
