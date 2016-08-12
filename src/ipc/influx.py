2172c06fa35ca36ecccd8fbd875a4428f523615a
cnettel <carl.nettelblad@icm.uu.se>
2016-08-12 09:31:13 -0700

    Influx fixes

--- src/ipc/influx.py
+++ src/ipc/influx.py
@@ -9,13 +9,17 @@ queue = None
 thread = None
 
 def init(dsn):
-    from influxdb import InfluxDBClusterClient
+    from influxdb import InfluxDBClient
     global client
     global thread
     global queue
-    client = InfluxDBClusterClient.from_DSN(dsn)
+    print "Initing InfluxDB"
+    client = InfluxDBClient.from_DSN(dsn)
+    print dsn
+    print "Inited InfluxDB"
     queue = Queue.Queue(10000)
     thread = threading.Thread(target = influxWorker)
+    thread.start()
 
 def influxWorker():
     while True:
@@ -24,13 +28,16 @@ def influxWorker():
         # i.e. grab all events there are and then send them
         block = True
         try:
+            print "Looping"
             while True:
                 data.append(queue.get(block))
                 block = False 
         except Queue.Empty:
             pass
 
+        print "Writing stuff"
         client.write_points(data)
+        print "Wrote stuff"
         # Explicit sleep to encourage multi-event writes and (hopefully) always keep the
         # InfluxDB backend respsonsive for visualization queries
         time.sleep(0.01) 
@@ -38,11 +45,11 @@ def influxWorker():
 def write(title, data_y, eventId, kwds):
     if client is None:
         return
-    data = [{"measurement": title,
+    data = {"measurement": title,
              "tags": {},
              "time": datetime.datetime.fromtimestamp(eventId, tz=timezone('utc')),
-             "fields": { "data": data_y} }]
+             "fields": { "data": data_y} }
     for key in kwds:
-        data[0]["fields"][key] = kwds[key]
-
+        data["fields"][key] = kwds[key]
+    
     queue.put(data)
