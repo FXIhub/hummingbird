d0843d50f3ed1ed930bdab309e3990251171b50e
Tomas Ekeberg <tekeberg@max-wng003.desy.de>
2017-02-23 15:26:27 +0100

    Added scanning plots to the configuration files.

--- conf_offline.py
+++ conf_offline.py
@@ -10,24 +10,30 @@ import numpy as np
 import time
 import ipc
 
+scanInjector = True
+scanXmin = 0
+scanXmax = 100
+scanXbins = 20
+scanZmin = 0
+scanZmax = 100
+scanZbins = 20
+
+outputEveryImage = False
+
 # Quick config parameters
-hitScoreThreshold = 9000
+# hitScoreThreshold = 9000
+# aduThreshold = 200
+hitScoreThreshold = 2000
 aduThreshold = 200
 
 # Specify the facility
 state = {}
 state['Facility'] = 'FLASH'
 # Specify folders with frms6 and darkcal data
-state['FLASH/DataGlob'] = '/gpfs/cfel/cxi/data/2016/FLASH-2016-Chapman-May/raw/pnccd/data/PS1_20160528_0608*.frms6'
-#state['FLASH/CalibGlob'] = '/gpfs/cfel/cxi/data/2016/FLASH-2016-Chapman-May/raw/pnccd/calib/'
-state['FLASH/CalibGlob'] = '/gpfs/cfel/cxi/data/2016/FLASH-2016-Chapman-May/raw/pnccd/calib/'
-#state['FLASH/DAQFolder'] = '/gpfs/cfel/cxi/data/2016/FLASH-2016-Chapman-May/raw/daq/chapman2016-05/'
-state['FLASH/DAQFolder'] = '/home/tekeberg/Beamtimes/Holography2017/fake_daq_data'
+state['FLASH/DataGlob'] = "/data/beamline/current/raw/pnccd/block-01/holography*.frms6"
+state['FLASH/CalibGlob'] = "/data/beamline/current/processed/calib/block-01/*.darkcal.h5"
+state['FLASH/DAQFolder'] = "/asap3/flash/gpfs/bl1/2017/data/11001733/processed/daq/"
 state['FLASH/MotorFolder'] = '/home/tekeberg/Beamtimes/Holography2017/motor_positions/fake_old_positions.data'
-#state['FLASH/DataGlob'] = '/var/acqu/bl1camp/Chapman_2016/CCD_Data/Databg_20160518_0622*.frms6'
-#state['FLASH/CalibGlob'] = '/var/acqu/bl1camp/Chapman_2016/CCD_Calib/*.darkcal.h5'
-#state['FLASH/MotorFolder'] = 'motors/stage-server/'
-#state['FLASH/DAQFolder'] = '/var/acqu/bl1camp/Chapman_2016/DAQ/'
 state['do_offline'] = True
 #state['FLASH/ProcessingRate'] = 1
 
@@ -35,13 +41,14 @@ def calculate_epoch_times(evt, time_sec, time_usec):
     add_record(evt['ID'], 'ID', 'time', time_sec.data + 1.e-6*time_usec.data)
     #add_record(evt['ID'], 'ID', 'timeAgo', time.time() - (time_sec.data + 1.e-6*time_usec.data))
     # Calculating timeAgo with 606 second offset due to miscalibration of pnCCD server clock
-    add_record(evt['ID'], 'ID', 'timeAgo', -606. + time.time() - (time_sec.data + 1.e-6*time_usec.data))
+    #add_record(evt['ID'], 'ID', 'timeAgo', -606. + time.time() - (time_sec.data + 1.e-6*time_usec.data))
+    add_record(evt['ID'], 'ID', 'timeAgo', 0. + time.time() - (time_sec.data + 1.e-6*time_usec.data))
 
 # This function is called for every single event
 # following the given recipe of analysis
 def onEvent(evt):
     # Processing rate [Hz]
-    analysis.event.printProcessingRate()
+    #analysis.event.printProcessingRate()
 
     # Calculate time and add to PlotHistory
     # calculate_epoch_times(evt, evt["ID"]["tv_sec"], evt["ID"]["tv_usec"])
@@ -53,18 +60,28 @@ def onEvent(evt):
                                        aduThreshold=aduThreshold, 
                                        hitscoreThreshold=hitScoreThreshold)
 
-    hit = evt["analysis"]["litpixel: isHit"].data
+    hit = bool(evt["analysis"]["litpixel: isHit"].data)
     plotting.line.plotHistory(evt["analysis"]["litpixel: hitscore"],
                               label='Nr. of lit pixels', hline=100, group='Metric')
     analysis.hitfinding.hitrate(evt, hit, history=5000)
 
-    plotting.histogram.plotHistogram(evt["analysis"]["litpixel: hitscore"],
-                                     hmin=30000, hmax=45000, bins=50,
-                                     name="Lit Pixel Histogram", group="Histograms")
-    plotting.image.plotImage(evt['photonPixelDetectors']['pnCCD'], name="pnCCD (All)", group='Images')
+    if scanInjector:
+        plotting.histogram.plotNormalizedHistogram(evt["motorPositions"]["InjectorX"], float(1 if hit else 0), hmin=scanXmin, hmax=scanXmax, bins=scanXbins, name="Histogram: InjectorX x Hitrate", group="Scan", buffer_length=1000)
+        plotting.histogram.plotNormalizedHistogram(evt["motorPositions"]["InjectorZ"], float(1 if hit else 0), hmin=scanZmin, hmax=scanZmax, bins=scanZbins, name="Histogram: InjectorZ x Hitrate", group="Scan", buffer_length=1000)
+
+        plotting.correlation.plotScatter(evt["motorPositions"]["InjectorX"], evt['analysis']['litpixel: hitscore'], 
+                                         name='InjectorX vs Hitscore', xlabel='InjectorX', ylabel='Hit Score',
+                                         group='Scan')
+        plotting.correlation.plotScatter(evt["motorPositions"]["InjectorZ"], evt['analysis']['litpixel: hitscore'], 
+                                         name='InjectorZ vs Hitscore', xlabel='InjectorZ', ylabel='Hit Score',
+                                         group='Scan')
+
+        
+    if outputEveryImage:
+        plotting.image.plotImage(evt['photonPixelDetectors']['pnCCD'], name="pnCCD (All)", group='Images')
 
     if ipc.mpi.is_main_worker():
-        plotting.line.plotHistory(evt["analysis"]["hitrate"], label='Hit rate [%]', group='Metric')
+        plotting.line.plotHistory(evt["analysis"]["hitrate"], label='Hit rate [%]', group='Metric', history=10000)
         # plotting.correlation.plotMeanMap(evt['motorPositions']['nozzle_x'], evt['motorPositions']['nozzle_y'],
         #                              #evt['analysis']['litpixel: hitscore'].data / 1e5, 
         #                              evt['analysis']['hitrate'].data, 
@@ -74,35 +91,4 @@ def onEvent(evt):
         #                              ylabel='nozzle_y (mm)',
         #                              group='Metric')
     if hit:
-        pass
-        # Visualize detector image if hit
         plotting.image.plotImage(evt['photonPixelDetectors']['pnCCD'], name="pnCCD (Hits)", group='Images')
-    
-    # Scatter plot
-    # plotting.correlation.plotScatter(evt['ID']['time'], evt['analysis']['litpixel: hitscore'], 
-    #                                  name='Hit Score vs t', 
-    #                                  xlabel='Epoch Time (s)', 
-    #                                  ylabel='Hit Score',
-    #                                  group='Metric')
-    # plotting.correlation.plotScatter(evt['ID']['time'], evt['ID']['timeAgo'], 
-    #                                  name='Delay vs time', 
-    #                                  xlabel='Epoch Time (s)', 
-    #                                  ylabel='Delay (s)',
-    #                                  group='Metric')
-    # plotting.correlation.plotScatter(evt['motorPositions']['nozzle_x'], evt['analysis']['litpixel: hitscore'], 
-    #                                  name='Hitscore vs nozzle_x', 
-    #                                  xlabel='nozzle_x (mm)', 
-    #                                  ylabel='Hit Score',
-    #                                  group='Metric')
-    # plotting.correlation.plotScatter(evt['motorPositions']['nozzle_y'], evt['analysis']['litpixel: hitscore'], 
-    #                                  name='Hitscore vs nozzle_y', 
-    #                                  xlabel='nozzle_y (mm)', 
-    #                                  ylabel='Hit Score',
-    #                                  group='Metric')
-
-    plotting.correlation.plotScatter(evt["motorPositions"]["InjectorX"], evt["motorPositions"]["InjectorZ"],
-                                     name="Injector X vs Z", xlabel="InjectorX", ylabel="InjectorZ",
-                                     group="Injector")
-    print("InjectorX = {0}".format(evt["motorPositions"]["InjectorX"].data))
-
-    # plotting.line.plotHistory(evt["ID"]["bunch_sec"], label="Bunch sec")
