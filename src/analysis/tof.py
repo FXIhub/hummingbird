5a35b28c3836de25fc614dcf4edc15c252daa9a4
amoopr <jana@slac.stanford.edu>
2017-09-22 05:05:40 -0700

    Missing data correction

--- src/analysis/tof.py
+++ src/analysis/tof.py
@@ -37,7 +37,7 @@ def tofPreproc(evt, type, key, baseline_region_guess, number_of_std=5, photon_pe
         diff_x = all_peak_x[1:] - all_peak_x[:-1]
         end_peak = all_peak_x[np.where(diff_x > 1)]
     else:
-        add_record(evt['analysis'], 'analysis', outkey, tof_trace_inverted-tof_peak_threshold)
+        add_record(evt['analysis'], 'analysis', outkey, tof_trace_inverted-(np.median(tof_trace_inverted[:baseline_region_guess])+tof_peak_threshold))
         return
 
     if photon_peak_position is None:
