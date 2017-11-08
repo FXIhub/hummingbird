6dcc083e4844798e1ab220b6c3de8383de860409
Benedikt Daurer <benedikt.daurer@icm.uu.se>
2017-11-08 16:44:34 +0100

    Removed image transformation (specific for FLASH) from the generic plotting module

--- src/plotting/image.py
+++ src/plotting/image.py
@@ -35,6 +35,4 @@ def plotImage(record, history=10, vmin=None, vmax=None, log=False, mask=None, ms
         image = image.reshape(sh[0]*sh[2], sh[1])
     if mask is None:
         mask = np.ones_like(image)
-    #ipc.new_data(n, image*mask, msg=msg, alert=alert, send_rate=send_rate)
-    ipc.new_data(n, np.fliplr(np.flipud(image.T*mask.T)), msg=msg, alert=alert, send_rate=send_rate)
-
+    ipc.new_data(n, image*mask, msg=msg, alert=alert, send_rate=send_rate)
