b46dfb5844bc6f5919c02089cb0b4ed6f07c73c4
cnettel <carl.nettelblad@it.uu.se>
2017-11-14 21:09:32 +0100

    More Karabo adaptations

--- src/backend/euxfel.py
+++ src/backend/euxfel.py
@@ -31,19 +31,46 @@ def add_cmdline_args():
     
 class EUxfelTranslator(object):
     """Translate between EUxfel events and Hummingbird ones"""
+    """Note: Karabo provides full trains. We extract pulses from those."""
     def __init__(self, state):
         self.timestamps = None
         
         cmdline_args = _argparser.parse_args()
         # TODO
-        #self.N = cmdline_args.lcls_number_of_frames          
-        if cmdline_args.lcls_run_number is not None:
-            dsrc += ":run=%i" % cmdline_args.lcls_run_number
+        #self.N = cmdline_args.euxfel_number_of_frames
+        self._zmq_context = zmq.Context()
+        self._zmq_request = self._zmq_context.socket(zmq.REQ)       
+        self._zmq_request.connect(cmdline_args.euxfel_socket)
+        self._num_read_ahead = 0
+        self._pos = 0
+        self._data = None
+        self._asked_data = False
+
+    def check_asked_data(self):
+        """"Call for new data if needed."""
+        if self._asked_data:
+            return
+
+        if self._data is None or self._pos >= self._data[self._mainsource][pulsecount] - self._num_read_ahead:
+            self._zmq_request.send(b'next')
+            self._asked_data = True
                     
     def next_event(self):
         """Grabs the next event and returns the translated version"""           
-        # TODO
-        return EventTranslator(evt, self)
+        # Old comment from Onda
+        # FM: When running with vetoeing we get data on cells 2,4,6...,28
+        # corresponding to indices 4,8,...,56
+        if self._data is None or self._pos == self._data[self._mainsource][pulsecount]:
+            self.check_asked_data()
+            self._data = self._zmq_request.recv_pyobj()
+            self._asked_data = False
+            self._pos = 0
+
+        self.checked_asked_data()
+        result = EventTranslator(evt, self)
+        
+        self._pos++
+        return result
 
     def event_keys(self, evt):
         """Returns the translated keys available"""
@@ -68,14 +95,14 @@ class EUxfelTranslator(object):
         return evt.keys()
 
     def translate(self, evt, key):
-        """Returns a dict of Records that match a given humminbird key"""
+        """Returns a dict of Records that match a given hummingbird key"""
         values = {}
         if(key in self._c2id_detectors):
             return self.translate_object(evt, key)
         elif(key in self._c2n):
             return self.translate_core(evt, key)
-        elif(key == 'parameters'):
-            return self._tr_epics()
+        #elif(key == 'parameters'):
+        #    return self._tr_epics()
         elif(key == 'analysis'):
             return {}
         elif(key == 'stream'):
@@ -123,51 +150,6 @@ class EUxfelTranslator(object):
                 add_record(values, det['type'], det['keys'][i], wf, ureg.V)
         else:
             raise RuntimeError('%s not yet supported' % key)
-        return values
-
-    def translate_core(self, evt, key):
-        # TODO
-        """Returns a dict of Records that matchs a core Hummingbird key.
-
-        Core keys include  all except: parameters, any psana create key,
-        any native key."""
-        values = {}
-        native_keys = self._c2n[key]
-        event_keys = evt.keys()
-        for k in event_keys:
-            if(k.type() in native_keys):
-                obj = evt.get(k.type(), k.src(), k.key())
-                if(isinstance(obj, psana.Bld.BldDataFEEGasDetEnergy) or
-                   isinstance(obj, psana.Bld.BldDataFEEGasDetEnergyV1)):
-                    self._tr_bld_data_fee_gas_det_energy(values, obj)
-                elif(isinstance(obj, psana.Lusi.IpmFexV1)):
-                    self._tr_lusi_ipm_fex(values, obj, k)
-                elif(key == 'photonEnergies'):
-                    self._tr_bld_data_ebeam(values, obj)
-                elif(isinstance(obj, psana.CsPad2x2.ElementV1)):
-                    self._tr_cspad2x2(values, obj)
-                elif(isinstance(obj, psana.CsPad.DataV2)):
-                    self._tr_cspad(values, obj, k)
-                # AMO
-                elif(isinstance(obj, psana.PNCCD.FullFrameV1)):
-                    self._tr_pnccdFullFrame(values, obj, k)
-                elif(isinstance(obj, psana.PNCCD.FramesV1)):
-                    self._tr_pnccdFrames(values, obj, k)
-                # --
-                elif(isinstance(obj, psana.Acqiris.DataDescV1)):
-                    self._tr_acqiris(values, obj, k)
-                elif(isinstance(obj, psana.Camera.FrameV1)):
-                    self._tr_camera(values, obj)
-                elif(isinstance(obj, psana.EventId)):
-                    self._tr_event_id(values, obj)
-                elif(isinstance(obj, psana.EvrData.DataV3) or
-                     isinstance(obj, psana.EvrData.DataV4)):
-                    self._tr_event_codes(values, obj)
-                else:
-                    print type(obj)
-                    print k
-                    raise RuntimeError('%s not yet supported' % (type(obj)))
-                
         return values
 
     def event_id(self, evt):
