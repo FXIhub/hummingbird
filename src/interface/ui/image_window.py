f374cfeb605e11a5324a5e19a88ef2bbf9c3d032
Tomas Ekeberg <tomas.ekeberg@gmail.com>
2019-05-22 23:59:36 +0200

    Fixed issue where hummingbird crashes when trying to view a trace as an image.

--- src/interface/ui/image_window.py
+++ src/interface/ui/image_window.py
@@ -451,7 +451,8 @@ class ImageWindow(DataWindow, Ui_imageWindow):
                 if not img.shape[0]:
                     continue
             elif conf["data_type"] == "vector":
-                img = numpy.array(pd.y[:,1,:], copy=False)
+                img = numpy.array(pd.y[:,:], copy=False)
+                #img = numpy.array(pd.y[:,1,:], copy=False)
                 bins   = img.shape[1]
                 hmin   = 0
                 hmax   = img.shape[1]
