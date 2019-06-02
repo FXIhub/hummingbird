9d39b3634bc02c1bc0630dc27059fe3f05a4bf50
Benedikt Daurer <daurerbe@exflonc06.desy.de>
2019-06-02 23:54:05 +0200

    Allow multiple processing rates with different labels

--- src/analysis/event.py
+++ src/analysis/event.py
@@ -9,19 +9,24 @@ import ipc
 import numpy as np
 from backend import EventTranslator
 
-processingTimes = collections.deque([], 1000)
-def printProcessingRate(pulses_per_event=1):
+#processingTimes = collections.deque([], 1000)
+processingTimesDict = {}
+def printProcessingRate(pulses_per_event=1, label="Processing Rate"):
     """Prints processing rate to screen"""
+    if label not in processingTimesDict:
+        processingTimesDict[label] = collections.deque([], 1000)
+    processingTimes = processingTimesDict[label]
     for i in range(pulses_per_event):
         processingTimes.appendleft(datetime.datetime.now())
     if(len(processingTimes) < 2):
         return
     dt = processingTimes[0] - processingTimes[-1]
     proc_rate = np.array((len(processingTimes)-1)/dt.total_seconds())
-    ipc.mpi.sum("processingRate", proc_rate)
+    # ipc.mpi.sum("processingRate", proc_rate)
+    ipc.mpi.sum(label, proc_rate)
     proc_rate = proc_rate[()]
     if(ipc.mpi.is_main_event_reader()):
-        print('Processing Rate %.2f Hz' % proc_rate)
+        print('{} {:.2f} Hz'.format(label, proc_rate))
 
 def printKeys(evt, group=None):
     """prints available keys of Hummingbird events"""
