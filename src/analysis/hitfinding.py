a388e19125b344b123cda724c1ecbb210ebff013
Kartik Ayyer <kartik.ayyer@desy.de>
2022-03-30 11:42:43 +0200

    hitfinding: Fixed dimensions of module stack

--- src/analysis/hitfinding.py
+++ src/analysis/hitfinding.py
@@ -89,7 +89,7 @@ def countLitPixels(evt, record, aduThreshold=20, hitscoreThreshold=200, hitscore
     
     if(mask is None):
         mask = 1
-    hitscore = ((record.data*mask) > aduThreshold).sum(axis=(0,1) if stack is True else None)
+    hitscore = ((record.data*mask) > aduThreshold).sum(axis=(1,2) if stack is True else None)
 
     hit = np.array(hitscore > hitscoreThreshold, dtype='int')
     if hitscoreMax is not None:
