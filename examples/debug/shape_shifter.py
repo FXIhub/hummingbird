87cfabdc68d42f1a269741a38780ed6c38e53cb1
Filipe Maia <filipe.c.maia@gmail.com>
2022-07-15 14:01:10 +0200

    Add a source with changing shape for debuging

---
+++ examples/debug/shape_shifter.py
@@ -0,0 +1,49 @@
+# Import analysis/plotting modules
+import analysis.event
+import plotting.image
+import numpy as np
+
+# Set new random seed
+np.random.seed()
+
+# Specify the facility
+state = {}
+state['Facility'] = 'Dummy'
+
+
+def shape_shifter():
+    if(np.random.random() < 0.5):
+        return np.random.rand(8,8)
+    else:
+        return np.random.rand(16,2)
+
+
+# Create a dummy facility
+state['Dummy'] = {
+    # The event repetition rate of the dummy facility [Hz]
+    'Repetition Rate' : 10,
+    # Dictionary of data sources
+    'Data Sources': {
+        # The name of the data source. 
+        'CCD': {
+            # A function that will generate the data for every event
+            'data': shape_shifter,
+            # The units to be used
+            'unit': 'ADU',     
+            # The name of the category for this data source.
+            # All data sources are aggregated by type, which is the key
+            # used when asking for them in the analysis code.
+            'type': 'photonPixelDetectors'
+        }        
+    }
+}
+
+# This function is called for every single event
+# following the given recipy of analysis
+def onEvent(evt):
+
+    # Processin rate [Hz]
+    analysis.event.printProcessingRate()
+
+    # Visualize detector image
+    plotting.image.plotImage(evt['photonPixelDetectors']['CCD'], send_rate=10)
