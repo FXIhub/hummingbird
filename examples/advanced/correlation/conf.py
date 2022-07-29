6637430aa7f6d58d4c1ac315ab524ebdd009c346
Filipe Maia <filipe.c.maia@gmail.com>
2022-07-29 18:46:42 +0200

    Fix more examples

--- examples/advanced/correlation/conf.py
+++ examples/advanced/correlation/conf.py
@@ -1,12 +1,13 @@
-import simulation.simple
+import simulation.condor
 import analysis.event
 import analysis.pixel_detector
 import analysis.hitfinding
 import plotting.line
 import plotting.image
 import plotting.correlation
+import numpy as np
 
-sim = simulation.simple.Simulation("examples/correlation/virus.conf")
+sim = simulation.condor.Simulation("examples/advanced/correlation/virus.conf")
 sim.hitrate = 0.1
 
 state = {
@@ -27,17 +28,17 @@ state = {
                 'type': 'pulseEnergies'
             },
             'injector_x': {
-                'data': sim.get_injector_x,
+                'data': lambda: np.random.random()*1e-6,
                 'unit': 'nm',
                 'type': 'parameters'
             },
             'injector_y': {
-                'data': sim.get_injector_y,
+                'data': lambda: np.random.random()*1e-6,
                 'unit': 'nm',
                 'type': 'parameters'
             },
             'injector_z': {
-                'data': sim.get_injector_z,
+                'data': lambda: np.random.random()*1e-6,
                 'unit': 'nm',
                 'type': 'parameters'
             }
@@ -73,33 +74,33 @@ def onEvent(evt):
     analysis.pixel_detector.printStatistics(evt["photonPixelDetectors"])
 
     # Count Nr. of Photons
-    analysis.pixel_detector.totalNrPhotons(evt,"photonPixelDetectors", "CCD")
+    analysis.pixel_detector.totalNrPhotons(evt,evt["photonPixelDetectors"]["CCD"], outkey="nrPhotons - CCD")
     plotting.line.plotHistory(evt["analysis"]["nrPhotons - CCD"], label='Nr of photons / frame', history=50)
 
     # Simple hitfinding (Count Nr. of lit pixels)
-    analysis.hitfinding.countLitPixels(evt, "photonPixelDetectors", "CCD", aduThreshold=0.5, hitscoreThreshold=10)
+    analysis.hitfinding.countLitPixels(evt, evt["photonPixelDetectors"]["CCD"], aduThreshold=0.5, hitscoreThreshold=10)
 
     # Compute the hitrate
-    analysis.hitfinding.hitrate(evt, evt["analysis"]["isHit - CCD"], history=100)
+    analysis.hitfinding.hitrate(evt, evt["analysis"]["litpixel: isHit"], history=100)
     
     # Plot the hitscore
-    plotting.line.plotHistory(evt["analysis"]["hitscore - CCD"], label='Nr. of lit pixels')
+    plotting.line.plotHistory(evt["analysis"]["litpixel: hitscore"], label='Nr. of lit pixels')
 
     # Plot the hitrate
     plotting.line.plotHistory(evt["analysis"]["hitrate"], label='Hit rate [%]')
      
     # Plot hit images
-    if evt["analysis"]["isHit - CCD"]:
+    if evt["analysis"]["litpixel: isHit"]:
         plotting.image.plotImage(evt["photonPixelDetectors"]["CCD"])
 
     # Plot MeanMap of hitrate(x,y)
     x = evt["parameters"]["injector_x"]
     y = evt["parameters"]["injector_y"]
-    z = evt["analysis"]["isHit - CCD"]
-    plotting.correlation.plotMeanMap(x,y,z, plotid='HitrateMeanMap', **hitrateMeanMap)
+    z = evt["analysis"]["litpixel: isHit"]
+    plotting.correlation.plotMeanMap(x,y,z, name='HitrateMeanMap', **hitrateMeanMap)
 
     # Scatter plot of Hitscore vs. Nr. scattered photons
-    x = evt["analysis"]["hitscore - CCD"]
+    x = evt["analysis"]["litpixel: hitscore"]
     y = evt["analysis"]["nrPhotons - CCD"]
     plotting.correlation.plotScatter(x,y, history=1000) 
 
