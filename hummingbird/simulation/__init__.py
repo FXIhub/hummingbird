fba1eed3fecb17a278d259d7c96e3ad2288f5cba
Egor Sobolev <egor.sobolev@xfel.eu>
2023-02-09 20:20:44 +0100

    Make module hummingbird.simulation.ptycho optional

--- hummingbird/simulation/__init__.py
+++ hummingbird/simulation/__init__.py
@@ -1,6 +1,11 @@
-from . import base, fromfile, ptycho, simulated_tof
+from . import base, fromfile, simulated_tof
 
 try:
     from . import condor
 except ModuleNotFoundError:
+    pass
+
+try:
+    from . import ptycho
+except:
     pass
